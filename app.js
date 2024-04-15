b237e73003e3a7663f368dbf851a9c439b785529
Kirshan Thakur <166369674+Thakurkirshana@users.noreply.github.com>
2024-04-15 23:54:57 +0500

    this is work

---
+++ app.js
@@ -0,0 +1,67 @@
+// data types
+
+//'hello world' // string
+
+//1,12,3 // integar
+// 1.2.3 // float
+ 
+
+
+
+//document.write( '<h2>this is the first class of javascript</h2>' )
+
+
+//document.write('<h2>'+'hello world'+'</h2>')
+
+//var abc = 'Kirshan Thakur'
+
+//alert(abc)//
+
+
+//alert('hello frinds')//
+
+//var firstName = 'Thakur'
+//var lastName = 'Kirshana'
+//document.write(firstName+'  '+lastName)
+
+//var b = prompt('Enter value 1')
+//var c = prompt('Enter value 2')
+//document.write(b%c)//
+//var add = b + c
+//var sub = b - c
+//var mul = b * c
+//var num1 = '5'
+//var num2 = 4
+//document.write(num1-num2)
+
+
+//+ k sign pe concat karega us soorat main jab string hogi 
+//+ k sing pe add karega jab dono integar hoon 
+//- sub karega jab 1 string or 1 interger ho tab bhi or 
+//dono int ho tab bhi same minu wali cheez mul div mod pe apply hogi
+//jab ek string ho usmain alphabet ho or ek int ho min mul div laga 
+//hoga to runtime pe decide karega or operation kardrga//
+
+
+
+
+
+
+
+
+var num1 = +prompt("Enter value 1")
+var num2 = +prompt("Enter value 2")
+
+var add = num1 +num2
+document.write(
+
+'<table border= "1px" >'+
+"<tr>"+
+'<th>'+"ADDITION"+'</th>'
++"</tr>"+
+'<td>'+ add + '</>'
++ "</tr>"
++"</table>"
+
+
+)
